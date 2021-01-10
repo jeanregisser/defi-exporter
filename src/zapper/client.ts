653c61e46b403761a43d293945c4d892fdde1fa5
Jean Regisser <jean.regisser@gmail.com>
2021-01-10 18:59:13 +0100

    Update zapper client to reflect latest api changes

--- src/zapper/client.ts
+++ src/zapper/client.ts
@@ -1,21 +1,25 @@
 import got from "got";
 
 const API_KEY = process.env.ZAPPER_API_KEY;
-const USER_AGENT =
-  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36";
 
 export async function fetchBalance<T>(
   service: string,
   address: string
 ): Promise<T> {
-  const url = `https://zapper.fi/api/account-balance/${service}?addresses=${address}`;
+  const url = `https://api.zapper.fi/v1/balances/${service}`;
 
-  const rawData: T = await got(url, {
-    headers: {
-      "api-key": API_KEY,
-      "user-agent": USER_AGENT,
-    },
-  }).json();
+  try {
+    const rawData: T = await got(url, {
+      searchParams: {
+        api_key: API_KEY,
+        "addresses[]": address,
+      },
+    }).json();
 
-  return rawData;
+    return rawData;
+  } catch (error) {
+    // Augment error object so the url is logged too (options is not enumerable and not logged by default)
+    error.url = error.options.url;
+    throw error;
+  }
 }
