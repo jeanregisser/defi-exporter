9059b94b5ebc1e6cb96591a024f55abaa9309f0e
Jean Regisser <jean.regisser@gmail.com>
2021-08-07 16:06:17 +0200

    Make zapper processing generic (no need to have 1 handler per protocol)

--- src/zapper/client.ts
+++ src/zapper/client.ts
@@ -2,17 +2,53 @@ import got from "got";
 
 const API_KEY = process.env.ZAPPER_API_KEY;
 
+namespace ZapperSupportedBalancesResponse {
+  export type Root = Network[];
+
+  export interface Network {
+    network: string;
+    protocols: Protocol[];
+  }
+
+  export interface Protocol {
+    protocol: string;
+    meta: Meta;
+  }
+
+  export interface Meta {
+    tags: string[];
+    supportedActions: string[];
+    label?: string;
+    img?: string;
+  }
+}
+
+export async function fetchSupportedBalances(address: string) {
+  const response: ZapperSupportedBalancesResponse.Root = await got(
+    `https://api.zapper.fi/v1/protocols/balances/supported`,
+    {
+      searchParams: {
+        api_key: API_KEY,
+        "addresses[]": address,
+      },
+    }
+  ).json();
+  return response;
+}
+
 export async function fetchBalance<T>(
-  service: string,
-  address: string
+  protocol: string,
+  address: string,
+  network?: string
 ): Promise<T> {
-  const url = `https://api.zapper.fi/v1/protocols/${service}/balances`;
+  const url = `https://api.zapper.fi/v1/protocols/${protocol}/balances`;
 
   try {
     const rawData: T = await got(url, {
       searchParams: {
         api_key: API_KEY,
         "addresses[]": address,
+        network,
       },
     }).json();
 
--- src/zapper/zapper.ts
+++ src/zapper/zapper.ts
@@ -1,24 +1,94 @@
 import { FastifyRequest } from "fastify";
-import { zapperBalancerHandler } from "./balancer";
-import { zapperCurveHandler } from "./curve";
-import { zapperTokensHandler } from "./tokens";
-import { zapperTokenSetsHandler } from "./tokenSets";
-import { zapperUniswapV2Handler } from "./uniswapV2";
-import { zapperYearnHandler } from "./yearn";
+import { getMetrics } from "../utils";
+import { fetchBalance, fetchSupportedBalances } from "./client";
+import { NAMESPACE } from "./consts";
 
 type CustomRequest = FastifyRequest<{
   Querystring: { address: any };
 }>;
 
+// Generic balance response (different protocols have more fields
+namespace ZapperBalanceResponse {
+  export interface Root {
+    [address: string]: Address | undefined;
+  }
+
+  export interface Address {
+    products: Product[];
+    meta: Meta[];
+  }
+
+  export interface Product {
+    label: string;
+    assets: Asset[];
+    meta: any[];
+  }
+
+  export interface Asset {
+    type: string;
+    category: string;
+    address: string;
+    symbol: string;
+    decimals: number;
+    label: string;
+    img: string;
+    hide: boolean;
+    canExchange: boolean;
+    price: number;
+    balance: number;
+    balanceRaw: string;
+    balanceUSD: number;
+  }
+
+  export interface Meta {
+    label: string;
+    value: number;
+    type: string;
+  }
+}
+
 export async function zapperHandler(req: CustomRequest) {
-  const result = await Promise.all([
-    zapperTokensHandler(req),
-    zapperUniswapV2Handler(req),
-    zapperBalancerHandler(req),
-    zapperTokenSetsHandler(req),
-    zapperCurveHandler(req),
-    zapperYearnHandler(req),
-  ]);
+  const { address } = req.query;
+  if (!address || typeof address !== "string") {
+    throw new Error("Address is required");
+  }
+
+  const supportedBalances = await fetchSupportedBalances(address);
+
+  const promises = [];
+
+  for (const { network, protocols } of supportedBalances) {
+    for (const { protocol } of protocols) {
+      promises.push(
+        (async () => {
+          const rawData = await fetchBalance<ZapperBalanceResponse.Root>(
+            protocol,
+            address,
+            network
+          );
+
+          const addressData = rawData[address]!.products.flatMap(
+            (product) => product.assets
+          );
+
+          const metrics = getMetrics(addressData, {
+            namespace: NAMESPACE,
+            keys: ["balance", "balanceUSD"],
+            labels: { network, protocol, address },
+            labelMappings: {
+              address: "assetAddress",
+              symbol: "assetName",
+              type: "assetType",
+            },
+          });
+
+          return metrics.join("\n");
+        })()
+      );
+    }
+  }
+
+  const result = await Promise.all(promises);
 
   return result.join("\n");
 }
