1dfce057a32d603e89ff991cf557dce37554ecca
Jean Regisser <jean.regisser@gmail.com>
2023-04-11 21:02:14 +0200

    [CoinGecko] Add timeout

--- src/coingecko.ts
+++ src/coingecko.ts
@@ -65,6 +65,7 @@ async function fetchMetrics(page: number) {
       page: page.toString(),
       price_change_percentage: "1h,24h,7d,14d,30d,200d,1y",
     },
+    timeout: 60_000,
   }).json();
 
   return rawData;
